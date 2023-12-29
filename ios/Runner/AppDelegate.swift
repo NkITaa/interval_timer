42f6f868b10aedcdd205a1422f044df0af480690
Nikita <reichert.nikita@gmail.com>
2023-12-29 08:37:26 +0100

    added name and icon for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,8 +4,6 @@ import Flutter
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
     
-    private let liveActivityManager: LiveActivityManager = LiveActivityManager()
-
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -13,31 +11,7 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
       
       let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-          let diChannel =  FlutterMethodChannel(name: "DI", binaryMessenger: controller.binaryMessenger)
 
-          diChannel.setMethodCallHandler({ [weak self] (
-                 call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-                      switch call.method {
-                      case "startLiveActivity":
-                          self?.liveActivityManager.startLiveActivity(
-                              data: call.arguments as? Dictionary<String,Any>,
-                              result: result)
-                          break
-                          
-                      case "updateLiveActivity":
-                          self?.liveActivityManager.updateLiveActivity(
-                              data: call.arguments as? Dictionary<String,Any>,
-                              result: result)
-                          break
-                          
-                      case "stopLiveActivity":
-                          self?.liveActivityManager.stopLiveActivity()
-                          break
-                          
-                      default:
-                          result(FlutterMethodNotImplemented)
-                  }
-             })
       
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
